875cb81269eb9121c7adaf68b94b8836651b8306
the48a48 <the48a48@gmail.com>
2025-04-24 16:50:34 +0200

    Update classe coup

--- src/Modele/Coup.java
+++ src/Modele/Coup.java
@@ -14,27 +14,37 @@ public class Coup {
 
     /**
      * renvoie vrai si deux coups sont égaux
-     * @param c : Coup à comparer
+     * @param c Coup à comparer
      * */
     public boolean equals(Coup c) {
         return getDepart().equals(c.getDepart()) && getArrivee().equals(c.getArrivee());
     }
 
     /**
-     * renvoie le Point correspondant
+     * renvoie le Point correspondant au point de départ du coup
+     * @return Le point de départ du coup
      * */
     public Point getDepart() {
         return this.depart;
     }
 
+    /**
+     * renvoie le Point correspondant à l'arrivée du coup
+     * @return Le point d'arrivée du coup
+     * */
     public Point getArrivee() {
         return this.arrivee;
     }
 
-    ///  renvoie la représentation textuelle d'un coup
+    /**
+     * renvoie la représentation textuelle d'un coup
+     * @return Un string représentant le coup
+     */
     public String toString() {
         StringBuilder S = new StringBuilder();
-        S.append(" ");
+        S.append("(").append(depart.getX()).append(",").append(depart.getY()).append(")");
+        S.append(" -> ");
+        S.append("(").append(arrivee.getX()).append(",").append(arrivee.getY()).append(")");
         return S.toString();
     }
 }
