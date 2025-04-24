1dd186ef1b7bd304229ed5370a072151e7131219
the48a48 <the48a48@gmail.com>
2025-04-24 16:42:43 +0200

    Implémentation de la classe Coup

--- src/Modele/Coup.java
+++ src/Modele/Coup.java
@@ -1,33 +1,40 @@
 package Modele;
 
+import java.awt.*;
 import java.util.ArrayList;
 
 public class Coup {
-    int i, j; // coin supérieur gauche du rectangle
-    ArrayList<int []> cases; // liste des cases affectées
+    private Point depart;
+    private Point arrivee;
 
-    public Coup(int i, int j) {
-        this.i = i;
-        this.j = j;
-        this.cases = new ArrayList<>();
+    public Coup(Point depart, Point arrivee) {
+        this.depart = depart;
+        this.arrivee = arrivee;
     }
 
+    /**
+     * renvoie vrai si deux coups sont égaux
+     * @param c : Coup à comparer
+     * */
     public boolean equals(Coup c) {
-        return this.i == c.i && this.j == c.j;
+        return getDepart().equals(c.getDepart()) && getArrivee().equals(c.getArrivee());
+    }
+
+    /**
+     * renvoie le Point correspondant
+     * */
+    public Point getDepart() {
+        return this.depart;
+    }
+
+    public Point getArrivee() {
+        return this.arrivee;
     }
 
     ///  renvoie la représentation textuelle d'un coup
     public String toString() {
         StringBuilder S = new StringBuilder();
-        S.append("__[").append(i).append(",").append(j).append("]__:");
-        S.append("{");
-        for (int i = 0; i < cases.size() - 1; i++) {
-            int [] ca = cases.get(i);
-            S.append("(").append(ca[0]).append(",").append(ca[1]).append("),");
-        }
-        int [] ca = cases.get(cases.size() - 1);
-        S.append("(").append(ca[0]).append(",").append(ca[1]).append(")");
-        S.append("}");
+        S.append(" ");
         return S.toString();
     }
 }
