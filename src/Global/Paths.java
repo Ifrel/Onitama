f08a68c7677c654efb04518f702d1ad425a8555a
MAKOUNDIKA <rhinelmak@gmail.com>
2025-05-03 15:59:49 +0200

    class contenant tous les chemins objets possibles (images, fichier, etc...)

---
+++ src/Global/Paths.java
@@ -0,0 +1,20 @@
+package Global;
+
+public class Paths {
+    public static String PATH_CARTE_GRENOUILLE  = "res/vue/images/cartes/GRENOUILLE.png";
+    public static String PATH_CARTE_ELEPHANT    = "res/vue/images/cartes/ELEPHANT.png";
+    public static String PATH_CARTE_SANGLIER    = "res/vue/images/cartes/SANGLIER.png";
+    public static String PATH_CARTE_ANGUILLE    = "res/vue/images/cartes/ANGUILLE.png";
+    public static String PATH_CARTE_DRAGON      = "res/vue/images/cartes/DRAGON.png";
+    public static String PATH_CARTE_CHEVAL      = "res/vue/images/cartes/CHEVAL.png";
+    public static String PATH_CARTE_BOEUF       = "res/vue/images/cartes/BOEUF.png";
+    public static String PATH_CARTE_TIGRE       = "res/vue/images/cartes/TIGRE.png";
+    public static String PATH_CARTE_MANTE       = "res/vue/images/cartes/MANTE.png";
+    public static String PATH_CARTE_SINGE       = "res/vue/images/cartes/SINGE.png";
+    public static String PATH_CARTE_CRABE       = "res/vue/images/cartes/CRABE.png";
+    public static String PATH_CARTE_LAPIN       = "res/vue/images/cartes/LAPIN.png";
+    public static String PATH_CARTE_COBRA       = "res/vue/images/cartes/COBRA.png";
+    public static String PATH_CARTE_GRUE        = "res/vue/images/cartes/GRUE.png";
+    public static String PATH_CARTE_COQ         = "res/vue/images/cartes/COQ.png";
+    public static String PATH_CARTE_OIE         = "res/vue/images/cartes/OIE.png";
+}
