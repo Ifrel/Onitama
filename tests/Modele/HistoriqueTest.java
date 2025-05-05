f204a8e0ac49158915bbc548d1d800ff48f9f03f
Arthur D'Herin <arthur.d-herin@etu.univ-grenoble-alpes.fr>
2025-05-05 22:27:01 +0200

    Modifs tests Historique

--- tests/Modele/HistoriqueTest.java
+++ tests/Modele/HistoriqueTest.java
@@ -2,6 +2,8 @@ package Modele;
 
 import org.junit.jupiter.api.Test;
 
+import java.awt.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HistoriqueTest<T> {
@@ -9,7 +11,7 @@ class HistoriqueTest<T> {
     public void peutAnnuler() {
         Historique<Coup> h = new Historique<>();
         assertFalse(h.peutAnnuler());
-        h.add(new Coup(1, 1));
+        h.add(new Coup(new Point(1, 2), new Point(45, 1234)));
         assertTrue(h.peutAnnuler());
     }
 
@@ -17,7 +19,7 @@ class HistoriqueTest<T> {
     public void peutRefaire() {
         Historique<Coup> h = new Historique<>();
         assertFalse(h.peutRefaire());
-        h.add(new Coup(1, 1));
+        h.add(new Coup(new Point(1, 2), new Point(45, 1234)));
         assertFalse(h.peutRefaire());
         h.annuler();
         assertTrue(h.peutRefaire());
@@ -26,7 +28,7 @@ class HistoriqueTest<T> {
     @Test
     public void annuler() {
         Historique<Coup> h = new Historique<>();
-        Coup c = new Coup(42, 42);
+        Coup c = new Coup(new Point(42, 23), new Point(26, 1234));
         h.add(c);
         assertEquals(h.annuler(), c);
     }
@@ -34,7 +36,7 @@ class HistoriqueTest<T> {
     @Test
     public void refaire() {
         Historique<Coup> h = new Historique<>();
-        Coup c = new Coup(42, 42);
+        Coup c = new Coup(new Point(42, 23), new Point(26, 1234));
         h.add(c);
         assertEquals(h.annuler(), c);
         assertEquals(h.refaire(), c);
