1dd2e8b14de4e4c985ef0a59ec320c69f8b02df7
MAKOUNDIKA <rhinelmak@gmail.com>
2025-05-10 06:09:50 +0200

    grosse mise à jour + implementations des methodes

--- src/Modele/PionMaitre.java
+++ src/Modele/PionMaitre.java
@@ -1,17 +1,74 @@
 package Modele;
 
-import Global.Config;
+import Global.Config.TYPE_ELEMENT_TERRAIN;
 import Global.Config.ROLEPION;
 
+import java.awt.*;
+import java.nio.file.Path;
 
-public class PionMaitre extends Pion {
-    boolean proprietaire;
+import static Global.Config.COULEUR_CASE_MAITRE_JOUEUR_1;
+import static Global.Config.COULEUR_CASE_MAITRE_JOUEUR_2;
+import static Global.Config.ROLEPION.*;
+import static Global.Config.TYPE_ELEMENT_TERRAIN.PION_MAITRE;
+import static Global.Paths.PATH_PION_BLEU_MAITRE;
+import static Global.Paths.PATH_PION_ROUGE_MAITRE;
 
-    public PionMaitre(boolean proprietaire) {
-        this.proprietaire = proprietaire;
+
+public class PionMaitre implements Pion {
+    private final int idJoueur;
+    private Path chemainImage;
+    private final TYPE_ELEMENT_TERRAIN type;
+    private Color couleur;
+    private final ROLEPION role;
+    private Point position;
+
+
+
+    public PionMaitre(int idJoueur, Point position) {
+        this.idJoueur = idJoueur;
+        this.type = PION_MAITRE;
+        this.role = Maitre;
+        this.position = position;
+
+        if (this.idJoueur == 1) {
+            this.couleur = COULEUR_CASE_MAITRE_JOUEUR_1;
+            this.chemainImage = PATH_PION_ROUGE_MAITRE;
+        }
+        else {
+            this.couleur = COULEUR_CASE_MAITRE_JOUEUR_2;
+            this.chemainImage = PATH_PION_BLEU_MAITRE;
+        }
     }
 
-    public Config.ROLEPION get_role() {
-        return Config.ROLEPION.Maitre; // or some custom logic
-    }
+
+
+
+
+
+    @Override
+    public ROLEPION getStatut() { return role; }
+
+    @Override
+    public Color getCouleur() { return couleur; }
+
+    @Override
+    public int getProprietaire() { return idJoueur; }
+
+    @Override
+    public Path getChemainImage() {  return chemainImage; }
+
+    @Override
+    public Point getPosition(){ return position; }
+
+    @Override
+    public TYPE_ELEMENT_TERRAIN getType() { return type;}
+
+    @Override
+    public void setNewPosition(Point position) {  this.position = position;  }
+
+    @Override
+    public void setCouleur(Color couleur) { this.couleur = couleur; }
+
+    @Override
+    public void setChemainImage(Path chemainImage) { this.chemainImage = chemainImage; }
 }
