d0d70518cdac8a905747c17fc1d16724a149355f
Rinel MAKOUNDIKA <makoundi@etu.univ-grenoble-alpes.fr>
2025-05-13 12:51:21 +0200

    Interface : casePlateau devient un singleton

--- src/Modele/CasePlateau.java
+++ src/Modele/CasePlateau.java
@@ -1,10 +1,16 @@
 package Modele;
 
+import Global.Config;
+import Vue.InfosDeConfigUI;
+
 import java.awt.*;
 
 /**
  * Représente une case du plateau de jeu, pouvant contenir un pion ou rien. */
 public class CasePlateau {
+    /**L'instance unique de la classe {@code CasePlateau}. Elle est initialisée à {@code null}    */
+    private static CasePlateau instance;
+
     Jeu jeu;
     Point position;
 
@@ -13,13 +19,31 @@ public class CasePlateau {
         PION_ETUDIANT,
         PION_MAITRE
     }
-
     private TYPE_ELEMENT_SUR_CASE typePion;
 
 
-    public CasePlateau(Jeu jeu, Point position){
+
+
+
+
+    private CasePlateau(Jeu jeu){
         this.jeu = jeu;
-        this.position = position;
+        this.position = new Point();
+        // Le constructeur est privé pour implémenter le pattern Singleton.
+    }
+
+
+
+    /**
+     * Retourne l'instance unique de la classe {@code CasePlateau}.
+     * Si aucune instance n'existe, une nouvelle est créée avant d'être retournée.
+     *
+     * @return L'instance unique de {@code CasePlateau}.     */
+    public static CasePlateau getInstance(Jeu jeu) {
+        if (instance == null) {
+            instance = new CasePlateau(jeu);
+        }
+        return instance;
     }
 
 
@@ -46,11 +70,24 @@ public class CasePlateau {
 
 
     public int getProprietaire() {
+        if (jeu.estCaseVide(position.x, position.y)) return 0;
         return jeu.getProprietairePionAt(position.x, position.y);
+    }
+
+    public Config.ROLEPION getRole(){
+        return  jeu.getRolePionAt(position.x, position.y);
     }
 
     public int getId() {
         return getProprietaire();
     }
+
+
+    public CasePlateau getCasePlateau(int row, int col) {
+        this.position.x = row;
+        this.position.y = col;
+        return instance;
+    }
+
 
 }
--- src/Modele/Jeu.java
+++ src/Modele/Jeu.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Random;
 
 import Global.Config.ROLEPION;
+import Vue.InfosDeConfigUI;
 
 import static Global.Config.*;
 import static Global.Config.ROLEPION.PION_ETUDIANT;
@@ -39,7 +40,7 @@ public class Jeu extends Observable {
     private final List<Pion> pionsJoueurUn = new ArrayList<>(); //Grille implicite: Liste de pions (chaque pion est associé à une position) du premier joueur
     private final List<Pion> pionsJoueurDeux = new ArrayList<>(); //idem pour le deuxième joueur
 
-
+    CasePlateau casePlateau ;
 
 
     public Jeu() {
@@ -60,6 +61,7 @@ public class Jeu extends Observable {
         numRound = 1;
         partieFinie = false;
 
+        casePlateau = CasePlateau.getInstance(this);
     }
 
 
@@ -444,8 +446,7 @@ public class Jeu extends Observable {
     }
 
     public CasePlateau getCasePlateau(int row, int col) {
-        //TODO
-        return new CasePlateau(this, new Point(row, col));
+        return casePlateau.getCasePlateau(row, col);
     }
 
     public Carte getCartesSurLeTerrain(int i) {
