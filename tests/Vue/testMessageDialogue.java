c2f2a8e0c2b6ab8a0f8c60709ad0a7a854333533
MAKOUNDIKA <rhinelmak@gmail.com>
2025-05-24 06:46:48 +0200

    IHM : Ajout test afficheur: fonctionalité en cours de dévélopement

---
+++ tests/Vue/testMessageDialogue.java
@@ -0,0 +1,26 @@
+package Vue;
+
+import org.junit.jupiter.api.Test;
+
+import javax.swing.*;
+
+import static Vue.Utils.MethodsStaticsUtils.afficherFonctionEnCours;
+
+public class testMessageDialogue {
+    @Test
+    public void principal() {
+        SwingUtilities.invokeLater(() -> {
+            JFrame cadre = new JFrame("Fenêtre principale");
+            cadre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            cadre.setSize(300, 150);
+            cadre.setLocationRelativeTo(null);
+
+            JButton bouton = new JButton("Tester le dialog");
+            bouton.addActionListener(e -> afficherFonctionEnCours());
+
+            cadre.add(bouton);
+            cadre.setVisible(true);
+        });
+    }
+}
+
