4690e900f466d88a9a0b75786933a3b0d8e23fa2
MAKOUNDIKA <rhinelmak@gmail.com>
2025-05-24 07:46:55 +0200

    IHM : mise à jour testMessageDialogue.java

--- tests/Vue/TestMessageDialogue.java
+++ tests/Vue/TestMessageDialogue.java
@@ -6,7 +6,7 @@ import javax.swing.*;
 
 import static Vue.Utils.MethodsStaticsUtils.afficherFonctionEnCours;
 
-public class testMessageDialogue {
+public class TestMessageDialogue {
     @Test
     public void principal() {
         SwingUtilities.invokeLater(() -> {
