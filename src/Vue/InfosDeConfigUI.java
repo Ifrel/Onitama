ebd6052e733af72a560b6c9300244fc98089a4e6
MAKOUNDIKA <rhinelmak@gmail.com>
2025-05-26 00:00:45 +0200

    IHM : mise à jour

--- src/Vue/InfosDeConfigUI.java
+++ src/Vue/InfosDeConfigUI.java
@@ -3,7 +3,6 @@ package Vue;
 import java.awt.*;
 import java.util.*;
 import java.util.List;
-import java.util.logging.Logger;
 
 import static Global.Config.*;
 
@@ -21,30 +20,16 @@ public class InfosDeConfigUI {
     /**
      * L'instance unique de la classe {@code InfosDeConfigUI}. Elle est initialisée à {@code null}    */
     private static InfosDeConfigUI instance;
-    private static final Logger logger = Logger.getLogger(InfosDeConfigUI.class.getName());
 
     private String nomCouleurPionJoueur1;
     private String nomCouleurPionJoueur2;
 
-    private Color couleurPionJoueur1;
-    private Color couleurPionJoueur2;
-    private Color couleurPlateauDejeu       = COULEUR_PLATEAU_DE_JEU;
-    private Color couleurCaseTerrain        = COULEUR_CASE_TERRAIN;
+    private final Color couleurPionJoueur1;
+    private final Color couleurPionJoueur2;
     private Color couleurCaseMaitreJoueur1  = COULEUR_CASE_MAITRE_JOUEUR_1;
     private Color couleurCaseMaitreJoueur2  = COULEUR_CASE_MAITRE_JOUEUR_2;
     private Color couleurCaseEleveJoueur1   = COULEUR_CASE_ELEVE_JOUEUR_1;
     private Color couleurCaseEleveJoueur2   = COULEUR_CASE_ELEVE_JOUEUR_2;
-    private Color couleurBlocMenu1          = COULEUR_BLOC_MENU;
-    private Color couleurBlocMenu2          = new Color(175, 175, 175);
-
-    private int vitesseAnimation            = 50;
-    private boolean animerDeplacementPiece  = true;
-    private boolean animerSurbrillace       = true;
-
-    private int volumeGeneral               = 50;
-    private int volumeEffetSonore           = 50;
-    private int volumeMusique               = 50;
-    private boolean couperToutSon           = false;
 
     // Pour la couleur aléatoire
     Color BLEU = new Color(26, 67, 104);
@@ -68,7 +53,6 @@ public class InfosDeConfigUI {
         this.nomsCouleurs.put(NOIRE, "noir");
         this.nomCouleurPionJoueur1 = nomsCouleurs.get(couleurPionJoueur1);
         this.nomCouleurPionJoueur2 = nomsCouleurs.get(couleurPionJoueur2);
-//        afficherEtatConfigUI();
         // Le constructeur est privé pour implémenter le pattern Singleton.
     }
 
@@ -94,61 +78,11 @@ public class InfosDeConfigUI {
     }
 
 
-
-    /**
-     * Définit la couleur du pion du joueur 1.
-     * @param nomCouleurPionJoueur1 La nouvelle couleur du pion du joueur 1.     */
-    public void setNomCouleurPionJoueur1(String nomCouleurPionJoueur1) {
-        this.nomCouleurPionJoueur1 = nomCouleurPionJoueur1;
-    }
-
-
     /**
      * Obtient la couleur du pion du joueur 2.
      * @return La couleur du pion du joueur 2 (une chaîne de caractères).     */
     public String getNomCouleurPionJoueur2() {
         return nomCouleurPionJoueur2;
-    }
-
-
-    /**
-     * Définit la couleur du pion du joueur 2.
-     * @param nomCouleurPionJoueur2 La nouvelle couleur du pion du joueur 2.    */
-    public void setNomCouleurPionJoueur2(String nomCouleurPionJoueur2) {
-        this.nomCouleurPionJoueur2 = nomCouleurPionJoueur2;
-    }
-
-
-
-    /**
-     * Obtient la couleur du plateau de jeu.
-     * @return La couleur du plateau de jeu (un objet {@code Color}).     */
-    public Color getCouleurPlateauDejeu() {
-        return couleurPlateauDejeu;
-    }
-
-
-    /**
-     * Définit la couleur du plateau de jeu.
-     * @param couleurPlateauDejeu La nouvelle couleur du plateau de jeu.     */
-    public void setCouleurPlateauDejeu(Color couleurPlateauDejeu) {
-        this.couleurPlateauDejeu = couleurPlateauDejeu;
-    }
-
-
-    /**
-     * Obtient la couleur des cases de terrain standard.
-     * @return La couleur des cases de terrain (un objet {@code Color}).     */
-    public Color getCouleurCaseTerrain() {
-        return couleurCaseTerrain;
-    }
-
-
-    /**
-     * Définit la couleur des cases de terrain standard.
-     * @param couleurCaseTerrain La nouvelle couleur des cases de terrain.     */
-    public void setCouleurCaseTerrain(Color couleurCaseTerrain) {
-        this.couleurCaseTerrain = couleurCaseTerrain;
     }
 
 
@@ -183,28 +117,11 @@ public class InfosDeConfigUI {
         this.couleurCaseMaitreJoueur2 = couleurCaseMaitreJoueur2;
     }
 
-
-    /**
-     * Obtient la couleur des cases où se trouvent les élèves du joueur 1.
-     * @return La couleur des cases des élèves du joueur 1 (un objet {@code Color}).    */
-    public Color getCouleurCaseEleveJoueur1() {
-        return couleurCaseEleveJoueur1;
-    }
-
-
     /**
      * Définit la couleur des cases où se trouvent les élèves du joueur 1.
      * @param couleurCaseEleveJoueur1 La nouvelle couleur des cases des élèves du joueur 1.     */
     public void setCouleurCaseEleveJoueur1(Color couleurCaseEleveJoueur1) {
         this.couleurCaseEleveJoueur1 = couleurCaseEleveJoueur1;
-    }
-
-
-    /**
-     * Obtient la couleur des cases où se trouvent les élèves du joueur 2.
-     * @return La couleur des cases des élèves du joueur 2 (un objet {@code Color}).     */
-    public Color getCouleurCaseEleveJoueur2() {
-        return couleurCaseEleveJoueur2;
     }
 
 
@@ -217,159 +134,16 @@ public class InfosDeConfigUI {
 
 
     /**
-     * Obtient la couleur principale des blocs de menu.
-     * @return La couleur principale des blocs de menu (un objet {@code Color}).     */
-    public Color getCouleurBlocMenu1() {
-        return couleurBlocMenu1;
-    }
-
-
-    /**
-     * Définit la couleur principale des blocs de menu.
-     * @param couleurBlocMenu1 La nouvelle couleur principale des blocs de menu.     */
-    public void setCouleurBlocMenu1(Color couleurBlocMenu1) {
-        this.couleurBlocMenu1 = couleurBlocMenu1;
-    }
-
-
-    /**
-     * Obtient la couleur secondaire des blocs de menu.
-     * @return La couleur secondaire des blocs de menu (un objet {@code Color}).     */
-    public Color getCouleurBlocMenu2() {
-        return couleurBlocMenu2;
-    }
-
-
-    /**
-     * Définit la couleur secondaire des blocs de menu.
-     * @param couleurBlocMenu2 La nouvelle couleur secondaire des blocs de menu.     */
-    public void setCouleurBlocMenu2(Color couleurBlocMenu2) {
-        this.couleurBlocMenu2 = couleurBlocMenu2;
-    }
-
-
-    /**
-     * Obtient la vitesse de l'animation.
-     * @return La vitesse de l'animation en millisecondes.     */
-    public int getVitesseAnimation() {
-        return vitesseAnimation;
-    }
-
-
-    /**
-     * Définit la vitesse de l'animation.
-     * @param vitesseAnimation La nouvelle vitesse de l'animation en millisecondes.     */
-    public void setVitesseAnimation(int vitesseAnimation) {
-        this.vitesseAnimation = vitesseAnimation;
-    }
-
-
-    /**
-     * Vérifie si l'animation du déplacement des pièces est activée.
-     * @return {@code true} si l'animation du déplacement des pièces est activée, {@code false} sinon.     */
-    public boolean isAnimerDeplacementPiece() {
-        return animerDeplacementPiece;
-    }
-
-
-    /**
-     * Définit l'état de l'animation du déplacement des pièces.
-     * @param animerDeplacementPiece {@code true} pour activer l'animation, {@code false} pour la désactiver.     */
-    public void setAnimerDeplacementPiece(boolean animerDeplacementPiece) {
-        this.animerDeplacementPiece = animerDeplacementPiece;
-    }
-
-
-    /**
-     * Vérifie si l'animation de surbrillance est activée.
-     * @return {@code true} si l'animation de surbrillance est activée, {@code false} sinon.    */
-    public boolean isAnimerSurbrillace() {
-        return animerSurbrillace;
-    }
-
-
-    /**
-     * Définit l'état de l'animation de surbrillance.
-     * @param animerSurbrillace {@code true} pour activer l'animation, {@code false} pour la désactiver.     */
-    public void setAnimerSurbrillace(boolean animerSurbrillace) {
-        this.animerSurbrillace = animerSurbrillace;
-    }
-
-
-    /**
-     * Obtient le volume général de l'application.
-     * @return Le volume général (un entier entre 0 et 100).    */
-    public int getConfigSonVolumeGeneral() {
-        return volumeGeneral;
-    }
-
-
-    /**
-     * Définit le volume général de l'application.
-     * @param volumeGeneral Le nouveau volume général (un entier entre 0 et 100).    */
-    public void setConfigSonVolumeGeneral(int volumeGeneral) {
-        this.volumeGeneral = volumeGeneral;
-    }
-
-
-    /**
-     * Obtient le volume de la musique de fond.
-     * @return Le volume de la musique (un entier entre 0 et 100).     */
-    public int getConfigSonVolumeMusique() {
-        return volumeMusique;
-    }
-
-
-
-    /**
-     * Définit le volume de la musique de fond.
-     * @param volumeMusique Le nouveau volume de la musique (un entier entre 0 et 100).     */
-    public void setConfigSonVolumeMusique(int volumeMusique) {
-        this.volumeMusique = volumeMusique;
-    }
-
-
-    /**
-     * Obtient le volume des effets sonores.
-     * @return Le volume des effets sonores (un entier entre 0 et 100).     */
-    public int getConfigSonVolumeEffets() {
-        return volumeEffetSonore;
-    }
-
-
-    /**
-     * Définit le volume des effets sonores.
-     * @param volumeEffetSonore Le nouveau volume des effets sonores (un entier entre 0 et 100).    */
-    public void setConfigSonVolumeEffets(int volumeEffetSonore) {
-        this.volumeEffetSonore = volumeEffetSonore;
-    }
-
-
-    /**
-     * Vérifie si tout le son est coupé.
-     * @return {@code true} si tout le son est coupé, {@code false} sinon.    */
-    public boolean getConfigSonMuet() {
-        return couperToutSon;
-    }
-
-
-    /**
-     * Définit l'état de la coupure du son.
-     * @param couperToutSon {@code true} pour couper tout le son, {@code false} pour le réactiver.     */
-    public void setCouperToutSon(boolean couperToutSon) {
-        this.couperToutSon = couperToutSon;
-    }
-
-    /**
      * Retourne la couleur du pion pour le joueur spécifié.
      *
      * @param id l'identifiant du joueur (1 ou 2)
      * @return la couleur du pion du joueur 1 si id vaut 1, sinon celle du joueur 2
      */
     public Color getCouleurPionJoueur(int id) {
-        if (id == 1) return getCouleurPionJoueur1();
+        if (id == ID_JOUEUR_1) return getCouleurPionJoueur1();
         else return getCouleurPionJoueur2();
     }
+
 
     /**
      * Retourne la couleur actuellement affectée au pion du joueur 1.
@@ -378,16 +152,6 @@ public class InfosDeConfigUI {
      */
     public Color getCouleurPionJoueur1() {
         return couleurPionJoueur1;
-    }
-
-
-    /**
-     * Définit la couleur à utiliser pour le pion du joueur 1.
-     *
-     * @param couleurPionJoueur1 la nouvelle couleur à affecter
-     */
-    public void setCouleurPionJoueur1(Color couleurPionJoueur1) {
-        this.couleurPionJoueur1 = couleurPionJoueur1;
     }
 
 
@@ -401,43 +165,80 @@ public class InfosDeConfigUI {
     }
 
 
+
     /**
-     * Définit la couleur à utiliser pour le pion du joueur 2.
+     * Obtient le nom de la couleur du pion pour le joueur spécifié.
      *
-     * @param couleurPionJoueur2 la nouvelle couleur à affecter
+     * @param idJoueur l'identifiant du joueur (1 ou 2)
+     * @return le nom de la couleur du pion du joueur (une chaîne de caractères)
      */
-    public void setCouleurPionJoueur2(Color couleurPionJoueur2) {
-        this.couleurPionJoueur2 = couleurPionJoueur2;
-    }
-
-
-    public void afficherEtatConfigUI(){
-        logger.info("\nnomCouleurPionJoueur1 :" + nomCouleurPionJoueur1+
-                        "\nnomCouleurPionJoueur2 :"+ nomCouleurPionJoueur2 +
-                        "\ncouleurPionJoueur1 :"+ couleurPionJoueur1 +
-                        "\ncouleurPionJoueur2 :" + couleurPionJoueur2 +
-                        "\ncouleurPlateauDejeu :" + couleurPlateauDejeu +
-                        "\ncouleurCaseTerrain :" + couleurCaseTerrain +
-                        "\ncouleurCaseMaitreJoueur1 :" + couleurCaseMaitreJoueur1 +
-                        "\ncouleurCaseMaitreJoueur2 :" + couleurCaseMaitreJoueur2 +
-                        "\ncouleurCaseEleveJoueur1 :" + couleurCaseEleveJoueur1 +
-                        "\ncouleurCaseEleveJoueur2 :" + couleurCaseEleveJoueur2 +
-                        "\ncouleurBlocMenu1 :" + couleurBlocMenu1 +
-                        "\ncouleurBlocMenu2 :" + couleurBlocMenu2 +
-                        "\nvitesseAnimation :" + vitesseAnimation +
-                        "\nanimerDeplacementPiece :" + animerDeplacementPiece +
-                        "\nanimerSurbrillace :" + animerSurbrillace +
-                        "\nvolumeGeneral :" + volumeGeneral +
-                        "\nvolumeEffetSonore :" + volumeEffetSonore +
-                        "\nvolumeMusique :" +  volumeMusique+
-                        "\ncouperToutSon :" + couperToutSon
-        );
-
-    }
-
-
     public String getNomCouleurPionJoueur(int idJoueur) {
-        if (idJoueur == 1) return getNomCouleurPionJoueur1();
+        if (idJoueur == ID_JOUEUR_1) return getNomCouleurPionJoueur1();
         else return getNomCouleurPionJoueur2();
+    }
+
+
+    /**
+     * Retourne la couleur de la case des élèves pour le joueur spécifié.
+     *
+     * @param idJoueur l'identifiant du joueur (1 ou 2)
+     * @return la couleur de la case des élèves du joueur spécifié (un objet {@code Color})
+     */
+    public Color getCouleurCaseEleveJoueur(int idJoueur) {
+        if (idJoueur == ID_JOUEUR_1) return getCouleurCaseEleveJoueur1();
+        else return getCouleurCaseEleveJoueur2();
+    }
+
+
+    /**
+     * Retourne la couleur des cases où se trouvent les élèves du joueur 1.
+     *
+     * @return la couleur des cases des élèves du joueur 1 (un objet {@code Color})
+     */
+    public Color getCouleurCaseEleveJoueur1() {
+        return couleurCaseEleveJoueur1;
+    }
+
+
+    /**
+     * Retourne la couleur des cases où se trouvent les élèves du joueur 2.
+     *
+     * @return la couleur des cases des élèves du joueur 2 (un objet {@code Color})
+     */
+    public Color getCouleurCaseEleveJoueur2() {
+        return couleurCaseEleveJoueur2;
+    }
+
+
+    /**
+     * Définit la couleur des cases associées aux élèves d'un joueur spécifié.
+     *
+     * @param idJoueur L'identifiant du joueur (1 pour le joueur 1, tout autre identifiant pour le joueur 2).
+     * @param couleur La nouvelle couleur des cases associées aux élèves du joueur.
+     */
+    public void setCouleurCaseEtudiantJoueur(int idJoueur, Color couleur) {
+        if (idJoueur == ID_JOUEUR_1) setCouleurCaseEleveJoueur1(couleur);
+        else setCouleurCaseEleveJoueur2(couleur);
+    }
+
+
+
+    /**
+     * Retourne une représentation sous forme de chaîne de caractères de l'état actuel
+     * de la configuration de l'interface utilisateur, incluant toutes les couleurs
+     * définies pour les différents éléments du jeu.
+     *
+     * @return une chaîne de caractères décrivant l'état de la configuration
+     */
+    @Override
+    public String toString() {
+        return  "\n   nomCouleurPionJoueur1 : " + nomCouleurPionJoueur1 +
+                "\n   nomCouleurPionJoueur2 : " + nomCouleurPionJoueur2 +
+                "\n      couleurPionJoueur1 : " + couleurPionJoueur1 +
+                "\n      couleurPionJoueur2 : " + couleurPionJoueur2 +
+                "\ncouleurCaseMaitreJoueur1 : " + couleurCaseMaitreJoueur1 +
+                "\ncouleurCaseMaitreJoueur2 : " + couleurCaseMaitreJoueur2 +
+                "\n couleurCaseEleveJoueur1 : " + couleurCaseEleveJoueur1 +
+                "\nncouleurCaseEleveJoueur2 : " + couleurCaseEleveJoueur2;
     }
 }
