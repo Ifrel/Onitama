79ca074e3ce407717bea1e553371ac62fa81fbda
MAKOUNDIKA <rhinelmak@gmail.com>
2025-05-26 03:44:58 +0200

    Refactor color handling logic for player-specific attributes
    Unified methods to manage player-specific color configurations, removing duplicate code. Introduced new getters and setters for clarity and added support for resetting all colors via a single method.
    
    Signed-off-by: MAKOUNDIKA <rhinelmak@gmail.com>

--- src/Controleur/ControleurEcranDeDemarrage.java
+++ src/Controleur/ControleurEcranDeDemarrage.java
@@ -3,10 +3,14 @@ package Controleur;
 import Global.Config;
 import Modele.Jeu;
 import Vue.CollecteurEvenements;
-import Vue.InfosDeConfigUI;
+import Vue.Configuration.InfosDeConfigUI;
 
 import java.awt.*;
 
+import static Global.Config.CiblesDesCouleurs.PION_TERRAIN_JOUEUR_1;
+import static Global.Config.CiblesDesCouleurs.PION_TERRAIN_JOUEUR_2;
+import static Global.Config.ID_JOUEUR_1;
+import static Global.Config.ID_JOUEUR_2;
 import static Global.Config.NIVEAU_IA.*;
 
 public class ControleurEcranDeDemarrage implements CollecteurEvenements {
@@ -66,19 +70,47 @@ public class ControleurEcranDeDemarrage implements CollecteurEvenements {
         System.err.println("couleur: " + couleur + " cible: " + cible);
         switch (cible){
             case CASE_ELEVE_JOUEUR_1:
-                infosDeConfigUI.setCouleurCaseEleveJoueur1(couleur);
+                infosDeConfigUI.setCouleurCaseEleveJoueur(ID_JOUEUR_1, couleur);
                 break;
             case CASE_ELEVE_JOUEUR_2:
-                infosDeConfigUI.setCouleurCaseEleveJoueur2(couleur);
+                infosDeConfigUI.setCouleurCaseEleveJoueur(ID_JOUEUR_2, couleur);
                 break;
             case CASE_MAITRE_JOUEUR_1:
-                infosDeConfigUI.setCouleurCaseMaitreJoueur1(couleur);
+                infosDeConfigUI.setCouleurCaseMaitreJoueur(ID_JOUEUR_1, couleur);
                 break;
             case CASE_MAITRE_JOUEUR_2:
-                infosDeConfigUI.setCouleurCaseMaitreJoueur2(couleur);
+                infosDeConfigUI.setCouleurCaseMaitreJoueur(ID_JOUEUR_2, couleur);
                 break;
             default:break;
         }
 
+     }
+
+    public String getNomCouleurPionJoueur(int idJoueur) {
+        return infosDeConfigUI.getNomCouleurPionJoueur(idJoueur);
+    }
+
+    public Color getCouleurPionJoueur(int idJoueur) {
+        return infosDeConfigUI.getCouleurPionJoueur(idJoueur);
+    }
+
+    public Color getCouleurCaseMaitreJoueur(int idJoueur) {
+        return infosDeConfigUI.getCouleurCaseMaitreJoueur(idJoueur);
+    }
+
+    public Color getCouleurCaseEleveJoueur(int idJoueur) {
+        return infosDeConfigUI.getCouleurCaseEleveJoueur(idJoueur);
+    }
+
+    public void setCouleurPion(Config.CiblesDesCouleurs cible, String nomCouleurPionJoueur) {
+        if (cible == PION_TERRAIN_JOUEUR_1) {
+            infosDeConfigUI.setCouleurPion(ID_JOUEUR_1, nomCouleurPionJoueur);
+        } else if (cible == PION_TERRAIN_JOUEUR_2) {
+            infosDeConfigUI.setCouleurPion(ID_JOUEUR_2, nomCouleurPionJoueur);
+        }
+    }
+
+    public void reinitialiserCouleurs() {
+        infosDeConfigUI.reinitialiserCouleurs();
     }
 }
